df3db7a74133b6ca52c45061f85f0b8512891080
Vaibhav Dhawan <vaibhav.dhawan@dominodatalab.com>
2021-01-12 12:58:58 +0000

    Update rdp.sh
    Limited fluxbox menu settings

--- supporting/rdp.sh
+++ supporting/rdp.sh
@@ -6,8 +6,10 @@ source $HOME/.rdp-defaults
 
 # Configure Fluxbox
 mkdir $HOME/.fluxbox
-echo "session.screen0.toolbar.visible: false" > $HOME/.fluxbox/init
-#echo "session.screen0.toolbar.tools:  iconbar, systemtray" > $HOME/.fluxbox/init
+#echo "session.screen0.toolbar.visible: false" > $HOME/.fluxbox/init
+echo "session.screen0.toolbar.tools:  iconbar, systemtray" > $HOME/.fluxbox/init
+echo "session.screen0.titlebar.left:  Stick" >> $HOME/.fluxbox/init 
+echo "session.screen0.titlebar.right: Maximize" >> $HOME/.fluxbox/init
 echo "background: none" > $HOME/.fluxbox/overlay
 
 # Launch Texmaker
